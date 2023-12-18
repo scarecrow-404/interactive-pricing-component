61e4658f3e7a36fe882bbbd4fb1f11b5cce853f3
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-12-18 11:53:30 +0700

    fix(ToggleSwitch):fix group hover

--- src/component/ToggleSwitch.jsx
+++ src/component/ToggleSwitch.jsx
@@ -8,15 +8,15 @@ const ToggleSwitch = ({ setIsYearly }) => {
   };
 
   return (
-    <label className="flex items-center cursor-pointer">
-      <div className="relative">
+    <label className="flex items-center cursor-pointer ">
+      <div className="relative group">
         <input
           type="checkbox"
-          className="sr-only "
+          className="sr-only hover:bg-[#10D5C2]"
           checked={isChecked}
           onChange={handleToggle}
         />
-        <div className="block hover:bg-[#10D5C2] bg-[#CDD7EE] w-10 h-6 rounded-full"></div>
+        <div className="block group-hover:bg-[#10D5C2] bg-[#CDD7EE] w-10 h-6 rounded-full"></div>
         <div
           className={`dot absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition ${
             isChecked ? "translate-x-full" : "translate-x-0"
